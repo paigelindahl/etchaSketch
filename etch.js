5a4af213098a75a610ce24175210db04dc7808db
Paige Lindahl <paige@peakfifteen.co.uk>
2022-01-23 20:53:08 +0000

    adds reset button

--- etch.js
+++ etch.js
@@ -1,4 +1,6 @@
 const container = document.getElementById("container");
+const resetButton = document.getElementById("reset-btn");
+const gridItems = document.getElementsByClassName("grid-item");
 
 const makeGrid = function(rows, cols) {
 
@@ -6,8 +8,18 @@ const makeGrid = function(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (let i = 0; i < (rows * cols); i++) {
     let cell = document.createElement('div');
+    cell.addEventListener('mouseover', function(event) {
+        event.target.classList.add('hovered');
+      })
     container.appendChild(cell).className = 'grid-item';
   }
 }
+
+resetButton.addEventListener('click', function() {
+  for(let item of gridItems) {
+    item.classList.remove("hovered");
+  }
+})
+
 
 makeGrid(16, 16);
